ad9c1f2ef22638b586a86c0f8b8445044032fd80
leosz2005 <leonardosanchezzerlin2005@gmail.com>
2024-03-13 20:49:34 +0100

    Primer componente de web

--- proyecto-react-sergi/src/PrimerComponente.jsx
+++ proyecto-react-sergi/src/PrimerComponente.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
 
-const string = 'Esto es un texto'
-const number = 123
-const array = [1, 2, 3]
-const boolean = true
-const funcion = (a, b) => a*b
-const objeto = { nombre: 'Curso de Javascript', duracion: 4 }
-const fecha = new Date()
-
-export const PrimerComponente = () => {
+export const PrimerComponente = ({titulo, subtitulo}) => {
+  console.log(titulo)
+  console.log(subtitulo)
+  
   return (
-    <h1>{funcion(5, 10)}</h1>
+    <>
+      <h1>{titulo}</h1>
+      <h2>{subtitulo}</h2>
+    </>
   )
 }
--- proyecto-react-sergi/src/main.jsx
+++ proyecto-react-sergi/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {PrimerComponente} from './PrimerComponente'
+import './style.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrimerComponente />
+    <PrimerComponente titulo={'Primer Componente'} subtitulo={'Subtitulo del primer componente'}/>
   </React.StrictMode>,
 )
